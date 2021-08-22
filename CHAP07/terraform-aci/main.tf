906fe81d22a469366549770b8cc0a41e0dad1219
jkvbe <jan.valvekens@invenit.be>
2021-08-22 12:04:36 +0200

    updated tf file

--- CHAP07/terraform-aci/main.tf
+++ CHAP07/terraform-aci/main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "=2.46.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = $(var.resource_group_name)
+    storage_account_name = $(var.storage_account_name)
+    container_name = $(var.container_name)
+    key = $(var.key)
+}
 }
 
 # Configure the Microsoft Azure Provider
