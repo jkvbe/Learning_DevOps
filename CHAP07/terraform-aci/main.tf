f84b8fc452e1f4255a78bbd8a12c8e2ecc331745
jkvbe <jan.valvekens@invenit.be>
2021-08-22 11:27:11 +0200

    updated tf file

--- CHAP07/terraform-aci/main.tf
+++ CHAP07/terraform-aci/main.tf
@@ -1,10 +1,15 @@
-provider "azurerm" {}
-
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
-  backend "azurerm" {}
-  required_version = ">= 0.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
