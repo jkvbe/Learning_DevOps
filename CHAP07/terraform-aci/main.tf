4ba5d79b612166ba0416e5bf8b81be2262abf722
jkvbe <jan.valvekens@invenit.be>
2021-08-24 08:53:15 +0200

    go to docker hub again

--- CHAP07/terraform-aci/main.tf
+++ CHAP07/terraform-aci/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = ">=2.46.0"
     }
   }
 
@@ -18,29 +18,29 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "acidemobook" {
-  name     = "demoBook"
-  location = "westus2"
+  name     = "demobook"
+  location = "westeurope"
 }
 
 variable "imageversion" {
   description = "Tag of the image to deploy"
 }
 
-#variable "dockerhub-username" {
-#  description = "DockerHub username"
-#}
+variable "dockerhub-username" {
+  description = "DockerHub username"
+}
 
 
 resource "azurerm_container_group" "aci-myapp" {
   name                = "aci-app"
-  location            = "West Europe"
+  location            = "westeurope"
   resource_group_name = azurerm_resource_group.acidemobook.name
   ip_address_type     = "public"
   dns_name_label      = "myapp-demo"
   os_type             = "linux"
   container {
     name   = "myappdemo"
-    image  = "devopslearning14321.azurecr.io/demobook:${var.imageversion}"
+    image  = "docker.io/${var.dockerhub-username}/demobook:${var.imageversion}"
     cpu    = "0.5"
     memory = "1.5"
 
